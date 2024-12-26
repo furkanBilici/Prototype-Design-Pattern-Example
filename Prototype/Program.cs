d0d113b2995e8f417e13f4edcf62ed284417557d
furkanBilici <fbilici258@gmail.com>
2024-12-26 19:44:00 +0300

    update

--- Prototype/Program.cs
+++ Prototype/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Prototype;
+
+Customer customer = new Customer { Name = "Furkan", Id = 13 ,order="Cheese Burger"};
+Customer customer2=(Customer) customer.Clone();
+customer2.Name = "Furkan Bilici";//klon değiştiriliyor
+
+Console.WriteLine(customer.Name);//nesnenin referansı değişmedi
+Console.WriteLine(customer2.Name);//Klon nesne değiştirildi
