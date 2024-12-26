6ce7a9173ac2924e606dc08a3f322186b5b89658
furkanBilici <fbilici258@gmail.com>
2024-12-26 19:30:43 +0300

    class and abstract update

---
+++ Prototype/Abstract/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype.Abstract
+{
+    public abstract class Person
+    {
+        public abstract Person GetPerson();
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Prototype/Customer.cs
@@ -0,0 +1,18 @@
+﻿using Prototype.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    internal class Customer : Person
+    {
+        public string order {  get; set; }
+        public override Person GetPerson()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
